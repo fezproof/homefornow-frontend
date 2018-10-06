6d2f2aecc2ecaf54dab313eeae3365fc2a3c454e
fezproof <stevebob1997@hotmail.com>
2018-10-06 15:34:41 +0800

    created a routing system added vuetify

--- src/router.js
+++ src/router.js
@@ -1,6 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+
+
+import Landing from './views/Landing.vue';
+
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+import TermsOfUse from './components/TermsOfUse.vue';
+import Contact from './components/Contact.vue';
 
 Vue.use(Router);
 
@@ -8,16 +15,30 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      name: 'landing',
+      component: Landing,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: Home,
+        },
+        {
+          path: '/about',
+          name: 'about',
+          component: About,
+        },
+        {
+          path: '/terms_of_use',
+          name: 'terms_of_use',
+          component: TermsOfUse,
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: Contact,
+        },
+      ],
     },
   ],
 });
