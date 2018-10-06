965d47e8d127d2f25da559c0311dad190203f31d
fezproof <stevebob1997@hotmail.com>
2018-10-07 03:09:15 +0800

    Added search capability

--- src/components/Landing/Home.vue
+++ src/components/Landing/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <v-layout row wrap style="height: 80vh">
+    <v-layout row wrap style="min-height: 80vh;">
       <v-flex xs12 sm6 offset-sm3>
         <v-card color="transparent" flat dark>
           <img src="img/logo.svg" type="image/svg+xml" class="image-center"/>
-          <v-card-title primary-title>
-            <div>
-              <div class="display-4">
+          <v-card-title>
+            <div style="margin: 0 auto;">
+              <div class="main-header">
                 Let's find a Home for Now
               </div>
               <div
@@ -19,8 +19,7 @@
           </v-card-title>
         </v-card>
       </v-flex>
-      <v-flex xs12 sm6 offset-sm3>
-      </v-flex>
+      <search/>
     </v-layout>
     <v-layout row wrap>
       <v-flex xs12 sm6 offset-sm3>
@@ -30,20 +29,27 @@
 </template>
 
 <script>
+import Search from '@/components/Landing/Search.vue';
 
 export default {
+  components: {
+    Search,
+  },
 };
 </script>
 
 <style scoped>
   .image-center {
     margin: 0 auto;
-    height: 200px;
+    height: calc(100px + 10vmin);
     display: block;
   }
 
-  .display-4 {
+  .main-header {
     font-family: 'K2D', sans-serif !important;
     text-align: center;
+    font-size: calc(40px + 4vmin);
+    letter-spacing: -0.02em !important;
+    line-height: 1.35 !important;
   }
 </style>
---
+++ src/components/Landing/Search.vue
@@ -0,0 +1,138 @@
+<template>
+  <v-flex xs12 sm6 offset-sm3>
+    <v-toolbar flat height="75">
+      <v-btn
+        icon
+        @click="geolocation"
+      >
+        <v-icon>my_location</v-icon>
+      </v-btn>
+      <v-input
+        single-line
+        :class="{ 'v-text-field': true, 'v-input--is-focused': focus, 'primary--text': focus }"
+        :error-messages="errors"
+      >
+        <div class="v-text-field__slot">
+          <gmap-autocomplete
+            v-model="location"
+            @place_changed="setPlace"
+            :options="mapOptions"
+            @focus.native="focus = true"
+            @blur.native="focus = false"
+          />
+        </div>
+      </v-input>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-flex>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      request: {
+        lat: '',
+        long: '',
+      },
+      errors: [],
+      location: '',
+      focus: false,
+      mapOptions: {
+        types: ['(cities)'],
+        componentRestrictions: { country: 'au' },
+      },
+    };
+  },
+  methods: {
+    async geolocation() {
+      const oldLocValue = this.location;
+      this.errors = [];
+      this.location = 'Finding your location ...';
+      try {
+        const position = await this.getCurrentPosition();
+        this.request.lat = position.coords.latitude;
+        this.request.long = position.coords.longitude;
+        const geocode = await this.geocode((''.concat(this.request.lat, ', ', this.request.long)));
+        const value = await this.getGeoVal(geocode.address_components);
+        this.location = value;
+      } catch (error) {
+        this.errors.push(error.message ? error.message : error);
+        this.location = oldLocValue;
+      }
+    },
+    async getCurrentPosition(options) {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+      });
+    },
+    async geocode(add) {
+      /* eslint-disable-next-line */
+      const geocoder = new google.maps.Geocoder();
+      return new Promise((resolve, reject) => {
+        geocoder.geocode({ address: add }, (results, status) => {
+        /* eslint-disable-next-line */
+          if (status === google.maps.GeocoderStatus.OK) {
+            resolve(results[0]);
+          } else {
+            reject(status);
+          }
+        });
+      });
+    },
+    async getGeoVal(addressComponents) {
+      let locality;
+      let state;
+      let country;
+      return new Promise((resolve, reject) => {
+        addressComponents.forEach((element) => {
+          if (!country && element.types[0] === 'country') {
+            country = element.long_name;
+          }
+          if (!state && element.types[0] === 'administrative_area_level_1') {
+            state = element.short_name;
+          }
+          if (!locality && element.types[0] === 'locality') {
+            locality = element.long_name;
+          }
+        });
+        if (country && state && locality) {
+          resolve(locality.concat(' ', state, ', ', country));
+        } else {
+          reject(new Error('Unable to retrieve location'));
+        }
+      });
+    },
+    async setPlace(place) {
+      this.request.lat = await place.geometry.location.lat();
+      this.request.long = await place.geometry.location.lng();
+    },
+  },
+};
+</script>
+
+<style>
+  .pac-container {
+    box-shadow: none;
+    border: none;
+    background: #f5f5f5;
+    border-radius: 0;
+  }
+
+  .pac-icon {
+    width: 0;
+    background-image: none;
+  }
+  .pac-item {
+    font-family: 'Roboto', sans-serif;
+    color: #333;
+    border: none;
+  }
+
+  .pac-item:hover, .pac-item-selected {
+    background: #efd5f6;
+  }
+</style>
